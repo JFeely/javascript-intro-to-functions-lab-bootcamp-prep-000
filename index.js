080d6e998a734beec2c0f6aa70f0e70e25fb94a2
Josephine Feely <jojofeely@yahoo.co.uk>
2019-03-17 18:58:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  console.log(string.toUpperCase)
-  return `YES INDEED!`
-}
-
-function sayHiToGrandma(string){
-  console.log(string.toLowerCase)
-  return `I can\'t hear you!`
-}
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase())
+  }
